b1c73c5938fce6cb3244ff24060a06c37f8eeb2c
Nathan Lam <nathanl1605@gmail.com>
2024-11-03 16:11:26 +0000

    general layout

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,10 +1,34 @@
 import React from "react";
 import Sidebar from "./Sidebar/Sidebar";
+import "./Dashboard.css";
 
 const Dashboard = () => {
     return(
         <>
             <Sidebar />
+            <div className="dashboard-container" style={{display: "flex", flexDirection: "column"}}>
+                <div className="header-container">
+                    <p className="header-txt">Dashboard</p>
+                </div>
+                <div className="summary-container" style={{display: "flex"}}>
+                    <div className="summaries">
+                        <p>Total Orders</p>
+
+                    </div>
+                    <div className="summaries">
+                        <p>Total Revenue</p>
+                    </div>
+                    <div className="summaries">
+                        <p>Total Customers</p>
+                    </div>
+                    <div className="summaries">
+                        <p>Booked Tables</p>
+                    </div>
+                </div>
+                <div className="main-container">
+                    
+                </div>
+            </div>
         </>
     );
 }
