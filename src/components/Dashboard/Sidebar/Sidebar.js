1e9af44c639925a338a07be87ab2ba6ef9e5fa03
Nathan Lam <nathanl1605@gmail.com>
2024-11-03 15:57:39 +0000

    logout functionality on dashboard

--- src/components/Dashboard/Sidebar/Sidebar.js
+++ src/components/Dashboard/Sidebar/Sidebar.js
@@ -1,8 +1,37 @@
 import React from "react";
+import "./Sidebar.css";
+import { CiLogout } from "react-icons/ci";
+import { useNavigate } from "react-router-dom";
+import Cookies from 'js-cookie';
+import { useEffect } from "react";
+import { useState } from "react";
 
 const Sidebar = () => {
+    const navigate = useNavigate(); 
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    useEffect(() => {
+        const idToken = Cookies.get('idToken');
+        if(idToken){
+            setIsLoggedIn(true);
+        }else{
+            setIsLoggedIn(false);   
+        }
+    }, []);
+
+    const handleLogout = () => {
+        Cookies.remove('idToken');
+        Cookies.remove('refreshToken');
+        setIsLoggedIn(false);
+        navigate('/');
+    };
     return(
-        <h1>Sidebar</h1>
+        <div className="sidebar">
+            <div className="home-btn-container">
+                <CiLogout className="logout-icon" onClick={handleLogout}/>
+            </div>
+
+        </div>
     )
 }
 
