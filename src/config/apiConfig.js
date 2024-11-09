a0e6fa48f0d2708a0318bd0ca2a0ce081ae9a8fa
Jakub Olszewski <j.olszewski05@gmail.com>
2024-11-09 00:19:27 +0000

    Added refresh token and axio interceptor to hanlde it autmatically

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,6 +1,31 @@
-// config.js
+import axios from 'axios';
+import authService from '../services/authService';
+
 const apiConfig = {
     baseURL: "https://us-central1-restaurant-management-sy-1a0cd.cloudfunctions.net/app"
 };
 
-export default apiConfig;
+const axiosInstance = axios.create({
+    baseURL: apiConfig.baseURL,
+});
+
+axiosInstance.interceptors.response.use(
+    response => response,
+    async error => {
+        const originalRequest = error.config;
+        if (error.response.status === 401 && !originalRequest._retry) {
+            originalRequest._retry = true;
+            try {
+                const newToken = await authService.refreshToken();
+                axiosInstance.defaults.headers.common['Authorization'] = `Bearer ${newToken}`;
+                originalRequest.headers['Authorization'] = `Bearer ${newToken}`;
+                return axiosInstance(originalRequest);
+            } catch (err) {
+                return Promise.reject(err);
+            }
+        }
+        return Promise.reject(error);
+    }
+);
+
+export default axiosInstance;
--- src/services/authService.js
+++ src/services/authService.js
@@ -1,10 +1,10 @@
-import axios from 'axios';
-import apiConfig from '../config/apiConfig';
+import axiosInstance from '../config/apiConfig';
 import cookieManager from '../utils/cookieManager';
 import COOKIE_KEYS from '../constants/cookieKeys';
+
 const authService = {
     login: async (email, password) => {
-        const response = await axios.post(`${apiConfig.baseURL}/auth/login`, {
+        const response = await axiosInstance.post(`/auth/login`, {
             email,
             password
         });
@@ -14,19 +14,37 @@ const authService = {
         return data;
     },
     register: async (formData) => {
+        const code = formData.countryCode;
         const updatedFormData = {
             ...formData,
-            phoneNumber: "+353" + formData.phoneNumber,
+            phoneNumber: code + formData.phoneNumber,
         };
 
-        const response = await axios.post(`${apiConfig.baseURL}/auth/register`, updatedFormData);
+        const response = await axiosInstance.post(`/auth/register`, updatedFormData);
         const data = response.data.data; 
         return data;
     },
     logout: async () => {
         cookieManager.remove(COOKIE_KEYS.ID_TOKEN);
         cookieManager.remove(COOKIE_KEYS.REFRESH_TOKEN);
+    },
+    refreshToken: async () => {
+        const refreshToken = cookieManager.get(COOKIE_KEYS.REFRESH_TOKEN);
+        if (!refreshToken) {
+            throw new Error('No refresh token available');
+        }
+
+        const response = await axiosInstance.post(`/auth/refreshToken`, {
+            refreshToken: refreshToken
+        });
+
+        const data = response.data.data;
+        cookieManager.set(COOKIE_KEYS.ID_TOKEN, data.idToken, { expires: 1 });
+        cookieManager.set(COOKIE_KEYS.REFRESH_TOKEN, data.refreshToken, { expires: 7 });
+
+        return data.idToken;
     }
+
 };
 
 export default authService;
