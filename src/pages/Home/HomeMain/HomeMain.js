a19db33ca080fc51b8ea9c5fbdf8aab00d204939
Nathan Lam <nathanl1605@gmail.com>
2024-12-31 16:09:49 +0000

    About us section complete

--- src/pages/Home/HomeMain/HomeMain.js
+++ src/pages/Home/HomeMain/HomeMain.js
@@ -4,6 +4,11 @@ import homeImage from "./homeImage.png";
 import { FaCalendarAlt } from "react-icons/fa";
 import { LuClock3 } from "react-icons/lu";
 import { BiSolidPhoneCall } from "react-icons/bi";
+import image2 from "./image2.jpg";
+import image22 from "./image22.jpg";
+import image3 from "./image3.jpg";
+import image33 from "./image33.jpg";
+import image333 from "./image333.jpg";
 
 const HomeMain = () => {
     return (
@@ -56,22 +61,83 @@ const HomeMain = () => {
                     </div>
 
                     <div className="openhours-text-btm">
-                        <p>Monday <span style={{color: "#FF7D05"}}> - </span> Friday: 9:00 <span style={{color: "#FF7D05"}}> AM </span> - 9:00 <span style={{color: "#FF7D05"}}> PM </span></p>
-                        <p>Saturday <span style={{color: "#FF7D05"}}> : </span> 10:00 <span style={{color: "#FF7D05"}}> AM </span> - 11:00 <span style={{color: "#FF7D05"}}> PM </span></p>
-                        <p>Sunday <span style={{color: "#FF7D05"}}> : </span> 10:00 <span style={{color: "#FF7D05"}}> AM </span> - 8:00 <span style={{color: "#FF7D05"}}> PM </span></p>
+                        <p>Monday <span style={{color: "#FF7D05"}}> - </span> Friday <span style={{color: "#FF7D05"}}> : </span> <br/>9:00 <span style={{color: "#FF7D05"}}> AM </span> - 9:00 <span style={{color: "#FF7D05"}}> PM </span></p>
+                        <br/><p>Saturday <span style={{color: "#FF7D05"}}> : </span> <br />10:00 <span style={{color: "#FF7D05"}}> AM </span> - 11:00 <span style={{color: "#FF7D05"}}> PM </span></p>
+                        <br/><p>Sunday <span style={{color: "#FF7D05"}}> : </span> <br />10:00 <span style={{color: "#FF7D05"}}> AM </span> - 8:00 <span style={{color: "#FF7D05"}}> PM </span></p>
                         <p className="phoneNumber-txt"><BiSolidPhoneCall className="phone-icon"/> <span className="phoneNumber">+123456789</span></p>
                     </div>
 
                 </div>
 
                 <div className="img-container2">
-                    <h1>test</h1>
+                    <img src={image2} />
+                    <img src={image22} />
                 </div>
             </div>
+            <div className="aboutus-container" style={{
+                marginBottom: "2rem",
+                width: "100%",
+                height: "auto",
+                padding: "2rem",
+                }}
+            >
+                <div className="aboutus-content-container">
+                    <h1 style={{color: "#FF7D05", marginBottom: "1rem"}}>About Us</h1>
 
-            
-            
-            
+                    <p>Welcome to RMS, where passion meets flavor. Nestled in the heart of RMS, our mission is to create unforgettable dining experiences. We believe that great food is the foundation of great memories, and our team is dedicated to serving dishes that delight your taste buds and warm your heart.</p>
+
+                    <p>At RMS, every ingredient tells a story. From locally sourced produce to globally inspired recipes, we craft each dish with care, precision, and love. Whether you&apos;re here for a casual meal, a special celebration, or just to unwind, we aim to provide a welcoming atmosphere that feels like home.</p>
+
+                    <p>Join us for a culinary journey filled with bold flavors, vibrant colors, and a dash of magic. Because here, food is more than nourishment — it&apos;s an experience.</p>
+
+                    <div className="img3-container" style={{paddingBottom: "6rem"}}>
+                        <img 
+                            src={image3} 
+                            alt="Food 1" 
+                            style={{
+                                position: "relative",
+                                zIndex: 3,
+                                left: "0",
+                                marginLeft: "0",
+                                width: "16.5rem",
+                                height: "auto",
+                            }}
+                        />
+                        <img 
+                            src={image33} 
+                            alt="Food 2" 
+                            style={{
+                                position: "relative",
+                                zIndex: 2,
+                                left: "-1rem",
+                                top: "6rem",
+                                marginLeft: "-30px",
+                                width: "12rem",
+                                height: "12rem",
+                            }}
+                        />
+                        <img 
+                            src={image333} 
+                            alt="Food 3" 
+                            style={{
+                                position: "relative",
+                                zIndex: 1,
+                                left: "-1rem",
+                                marginLeft: "-30px",
+                                width: "20rem",
+                                height: "auto",
+                            }}
+                        />
+                    </div>
+                </div>
+                
+
+                
+            </div>
+
+
+
+
         </div>
     );
 }
