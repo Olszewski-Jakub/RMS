53928728adda7381380e218cec6145371323077c
Nathan Lam <nathanl1605@gmail.com>
2025-01-07 19:06:25 +0000

    Location UI added

--- src/components/HomeHeader/HomeHeader.js
+++ src/components/HomeHeader/HomeHeader.js
@@ -30,7 +30,7 @@ const HomeHeader = () => {
                 navigate(ROUTES.MENU);
                 break;
             case "Location":
-                navigate("/");
+                navigate(ROUTES.LOCATION);
                 break;
             default:
                 navigate("/");
